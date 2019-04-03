c9ce2093ff0f38cc42fede561c1219183bec9c71
Yao ZHANG <yao.zhang@greenwaves-technologies.com>
2019-04-03 08:45:07 -0700

    update include and lib

--- include/vega_config.h
+++ include/vega_config.h
@@ -1,11 +1,13 @@
-#ifndef __GAP_CONFIG_H__
-#define __GAP_CONFIG_H__
+#ifndef __VEGA_CONFIG_H__
+#define __VEGA_CONFIG_H__
 
 #include "archi/pulp_defs.h"
 
 #define ARCHI_HAS_FC 1
 #define PLP_HAS_FC 1
-#define ARCHI_NB_PE 8
+#define PLP_NO_BUILTIN 1
+#define __RT_ALLOC_L2_MULTI 1
+#define ARCHI_NB_PE 9
 #define __BENCH__ 1
 #define BENCH 1
 #define PULP_RT_VERSION_RELEASE 0
@@ -14,14 +16,14 @@
 #define PULP_RT_VERSION_PROFILE1 3
 #define PULP_RT_VERSION_DEBUG 4
 #define PULP_RT_VERSION PULP_RT_VERSION_BENCH
-#define __RT_USE_IO 1
 #define PULP
 #define __PULP__
 #define __ri5cy__ 1
-#define PULP_CHIP CHIP_GAP
-#define PULP_CHIP_STR gap
-#define PULP_CHIP_FAMILY CHIP_GAP
-#define PULP_CHIP_FAMILY_STR gap
+#define PULP_CHIP CHIP_VEGA
+#define CONFIG_VEGA 1
+#define PULP_CHIP_STR vega
+#define PULP_CHIP_FAMILY CHIP_VEGA
+#define PULP_CHIP_FAMILY_STR vega
 #define PULP_CHIP_VERSION 0
 #define PULP_CORE CORE_RISCV_V4
 #define FP_SW_EMUL 1
